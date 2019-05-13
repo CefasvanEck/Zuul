1d4f5a1c4a1a49b28ca65afcacc90fdb31b6c0cb
CefasvanEck <50441721+CefasvanEck@users.noreply.github.com>
2019-05-13 09:37:12 +0200

    Revert "Revert "V1.0.0""
    This reverts commit e4ecb2bbc75dc04d3b641b448900cc758e1cf2db.

---
+++ ConsoleApp1/ConsoleApp1/Room.cs
@@ -0,0 +1,86 @@
+using System.Collections.Generic;
+
+namespace ZuulCS
+{
+	public class Room
+	{
+		private string description;
+		private Dictionary<string, Room> exits; // stores exits of this room.
+
+		/**
+	     * Create a room described "description". Initially, it has no exits.
+	     * "description" is something like "in a kitchen" or "in an open court
+	     * yard".
+	     */
+		public Room(string description)
+		{
+			this.description = description;
+			exits = new Dictionary<string, Room>();
+		}
+
+		/**
+	     * Define an exit from this room.
+	     */
+		public void setExit(string direction, Room neighbor)
+		{
+			exits[direction] = neighbor;
+		}
+
+		/**
+	     * Return the description of the room (the one that was defined in the
+	     * constructor).
+	     */
+		public string getShortDescription()
+		{
+			return description;
+		}
+
+		/**
+	     * Return a long description of this room, in the form:
+	     *     You are in the kitchen.
+	     *     Exits: north west
+	     */
+		public string getLongDescription()
+		{
+			string returnstring = "You are ";
+			returnstring += description;
+			returnstring += ".\n";
+			returnstring += getExitstring();
+			return returnstring;
+		}
+
+		/**
+	     * Return a string describing the room's exits, for example
+	     * "Exits: north, west".
+	     */
+		private string getExitstring()
+		{
+			string returnstring = "Exits:";
+
+			// because `exits` is a Dictionary, we can't use a `for` loop
+			int commas = 0;
+			foreach (string key in exits.Keys) {
+				if (commas != 0 && commas != exits.Count) {
+					returnstring += ",";
+				}
+				commas++;
+				returnstring += " " + key;
+			}
+			return returnstring;
+		}
+
+		/**
+	     * Return the room that is reached if we go from this room in direction
+	     * "direction". If there is no room in that direction, return null.
+	     */
+		public Room getExit(string direction)
+		{
+			if (exits.ContainsKey(direction)) {
+				return (Room)exits[direction];
+			} else {
+				return null;
+			}
+
+		}
+	}
+}
