8dca534558830340c4379f4bb85525c8cb4ab998
CefasvanEck <50441721+CefasvanEck@users.noreply.github.com>
2019-05-24 09:35:44 +0200

    V1.1.2
    Add player health with damage and heal functions

--- ConsoleApp1/ConsoleApp1/Player.cs
+++ ConsoleApp1/ConsoleApp1/Player.cs
@@ -3,10 +3,75 @@ namespace ZuulCS
     public class Player
     {
         private Room currentRoom;
+        /*
+         * The player health is a float so the player can take damage of half a heart, higher or lower.
+         * This way you can add as an example small enemy's that do little damage but when the enemy is in a big group they can take down the player.
+         * You can implement many damage scenario's with enemy's or objects.
+         */
+        private float maxHealth;
+        private float currentHealth;
 
         public Player()
         {
+            this.maxHealth = 20;
+            this.currentHealth = this.maxHealth;
+        }
 
+        public float getMaxHealth()
+        {
+            return this.maxHealth;
+        }
+
+        public float getCurrentHealth()
+        {
+            return this.currentHealth;
+        }
+
+        public void setCurrentHealth(float health)
+        {
+            this.currentHealth = health;
+        }
+
+        /**
+        * Add health to the player or set it to the max health(Player cant have more health then the max health.
+        */
+        public void heal(float amount)
+        {
+            if(this.currentHealth + amount <= this.maxHealth)
+            {
+                this.currentHealth += amount;
+            }
+            else
+            {
+                this.currentHealth = 20;
+            }
+        }
+
+        /*
+         * Damage the player or set the health to 0.
+         */
+        public void damage(float amount)
+        {
+            if (this.currentHealth - amount >= this.maxHealth)
+            {
+                this.currentHealth -= amount;
+            }
+            else
+            {
+                this.currentHealth = 0;
+            }
+        }
+
+        /*
+         * Checks if the player has any health left.
+         */
+        public bool isAlive()
+        {
+            if (this.currentHealth > 0)
+            {
+                return true;
+            }
+            else return false;
         }
 
         public void setCurrentRoom(Room room)
